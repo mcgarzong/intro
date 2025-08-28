805628092fac8ad8d3fcd7e3ddd241c510f0e5ee
mcgarzong <mcgarzong@eafit.edu.co>
2025-08-28 10:08:42 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
 import streamlit as st
+from PIL import Image
 
+st.title(" Mi primera app ")
 
-st.title("Mi primera app")
+st.header(" En este espacio comienzo a desarrollar mis aplicaciones para interfaces multimodales ")
+st.write(" Facilmente puedo realizar backend y fronted ")
+image = Image.open('dibujo.jpg')
+
+st.image(image, caption='Interfaces multimodales')
